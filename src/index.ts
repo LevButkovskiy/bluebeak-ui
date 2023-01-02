c972a4f70740e06be909555ca4275a6f02b10a51
ButkovskiyLev <lebutkovskiy@maximum.auto>
2023-01-02 10:07:04 +0300

    styles fix

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import Header from "./components/Header"
 import JSONPreview from "./components/JSONPreview"
 import Card from "./components/Card"
 import Container from "./components/Container"
+import Overview from "./components/Overview"
 
-export {Button, Header, JSONPreview, Card, Container}
+export {Button, Header, JSONPreview, Card, Container, Overview}
